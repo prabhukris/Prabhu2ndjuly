b0dcddddd2c1dcfa462e6eb3618f326723ad51ad
prabhukris <prabhu241996@gmail.com>
2021-07-02 09:30:34 +0530

    my first push

---
+++ src/test/java/com/framework/intro/Day1.java
@@ -0,0 +1,16 @@
+package com.framework.intro;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Day1 {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Prabhu Logesh\\eclipse-workspace\\OnlineFrameWorkMay24\\Drive\\chromedriver.exe");
+		 WebDriver driver = new ChromeDriver();
+		 
+		 driver.get("https://www.facebook.com/");
+		 driver.manage().window().maximize();
+		 
+	}
+
+}
